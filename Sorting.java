ca4849cabddd3d83688c2da0c8c81f6cb5bc2f53
Chad <csawyer2024@chadwickschool.org>
2023-09-12 12:00:37 -0700

    documentation

--- Sorting.java
+++ Sorting.java
@@ -3,196 +3,206 @@ public class Sorting
     private static long comparisons = 0;
     private static long swaps = 0;
     
-    public static void bubbleSort(int[] list)
-    {
-        long sCount = 0;
-        long cCount = 0;
-        for (int i = 0; i < list.length - 1; i++)
-        {
-            for (int j = 0; j < list.length - 1; j++)
-            {
-                cCount++;
-                if (list[j] > list[j + 1])
-                {
-                    sCount++;
-                    int temp = list[j];
-                    list[j] = list[j + 1];
-                    list[j + 1] = temp;
+    /**
+    * Method that sorts an array by pushing higher values below values in
+    * the array that are lower
+    */
+    public static void bubbleSort(int[] arr) {
+        long swaps = 0;
+        long comparisons = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                comparisons++;
+                
+                // Swaps if needed
+                if (arr[j] > arr[j + 1]) {
+                    swaps++;
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
-        System.out.print(", " + cCount + ", " + sCount);
-    }
-    
-    public static void selectionSort(int[] list)
-    {
-        long sCount = 0;
-        long cCount = 0;
-        for (int i = 0; i < list.length - 1; i++)
-        {
-            int min = list[i];
-            int location = i;
-            for (int j = i + 1; j < list.length; j++)
-            {
-                cCount++;
-                if (list[j] < min)
-                {
-                    min = list[j];
-                    location = j;
-                }
-            }
-            sCount++;
-            if (location != i)
-            {
-                cCount++;
-                int temp = min;
-                list[location] = list[i];
-                list[i] = temp;
-            }
-        }
-        System.out.print(", " + cCount + ", " + sCount);
-    }
-    
-    public static void insertionSort(int[] list)
-    {
-        long sCount = 0;
-        long cCount = 0;
-        for (int i = 1; i < list.length; i++)
-        {
-            int j = i;
-            cCount++;
-            while (j != 0 && list[j] < list[j - 1])
-            {
-                sCount++;
-                cCount++;
-                int temp = list[j];
-                list[j] = list[j - 1];
-                list[j - 1] = temp;
-                j--;
-            }
-        }
-        System.out.print(", " + cCount + ", " + sCount);
-    }
-    
-    public static void mergeSort(int[] list)
-    {
-        mergeSort(0, list.length - 1, list);
         System.out.print(", " + comparisons + ", " + swaps);
     }
     
-    public static void mergeSort(int beg, int end, int[] list)
-    {
-        if (beg == end)
-        {
-            return;
+    /**
+    * Method that sorts by swapping values in an array with the value that
+    * is supposed to be at that index when sorted
+    */
+    public static void selectionSort(int[] arr) {
+        long swaps = 0;
+        long comparisons = 0;
+        
+        // Iterates through array to allow swaps if necessary
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = arr[i];
+            int location = i;
+            
+            // Iterates through array to find minimum value after index
+            for (int j = i + 1; j < arr.length; j++) {
+                comparisons++;
+                if (arr[j] < min) {
+                    min = arr[j];
+                    location = j;
+                }
+            }
+            comparisons++;
+            
+            // Swaps value with the minimum value after it
+            if (location != i) {
+                swaps++;
+                int temp = min;
+                arr[location] = arr[i];
+                arr[i] = temp;
+            }
         }
-        int mid = (beg + end) / 2;
-        mergeSort(beg, mid, list);
-        mergeSort(mid + 1, end, list);
-        merge(beg, mid, end, list);
+        System.out.print(", " + comparisons + ", " + swaps);
     }
     
-    public static void merge(int beg, int mid, int end, int[] list)
-    {
-        int i = beg;
+    /**
+    * Method that sorts by swapping values with all higher preceding values
+    */
+    public static void insertionSort(int[] arr) {
+        long swaps = 0;
+        long comparisons = 0;
+        for (int i = 1; i < arr.length; i++) {
+            int j = i;
+            comparisons++;
+            
+            // Swaps a value if lower than preceding value in array
+            while (j != 0 && arr[j] < arr[j - 1]) {
+                swaps++;
+                comparisons++;
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+                j--;
+            }
+        }
+        System.out.print(", " + comparisons + ", " + swaps);
+    }
+    
+    /**
+    * Method that initiates merge sort for an array
+    */
+    public static void mergeSort(int[] arr) {
+        mergeSort(0, arr.length - 1, arr);
+        System.out.print(", " + comparisons + ", " + swaps);
+    }
+    
+    /**
+    * Method that sorts by splitting an array into individual pieces and
+    * merges the pieces into a sorted order
+    */
+    public static void mergeSort(int start, int end, int[] arr) {
+        // Base case
+        if (start == end) {
+            return;
+        } else {
+            int mid = (start + end) / 2;
+            mergeSort(start, mid, arr);
+            mergeSort(mid + 1, end, arr);
+            merge(start, mid, end, arr);
+        }
+    }
+    
+    /**
+    * Merges a split array into a sorted order
+    */
+    public static void merge(int start, int mid, int end, int[] arr) {
+        int i = start;
         int j = mid + 1;
-        int[] newArr = new int[end - beg + 1];
+        int[] newArr = new int[end - start + 1];
         int location = 0;
-        while (i <= mid && j <= end)
-        {
-            if (list[i] < list[j])
-            {
-                newArr[location] = list[i];
+        
+        /**
+        * Determines which piece of the array has a lower value and adds that
+        * to the correct index of the sorted array
+        */
+        while (i <= mid && j <= end) {
+            if (arr[i] < arr[j]) {
+                newArr[location] = arr[i];
                 i++;
                 location++;
                 comparisons++;
                 swaps++;
-            }
-            else
-            {
-                newArr[location] = list[j];
+            } else {
+                newArr[location] = arr[j];
                 j++;
                 location++;
                 comparisons++;
                 swaps++;
             }
         }
-        while (i <= mid)
-        {
-            newArr[location] = list[i];
+        
+        /**
+        * Adds the remaining values of a half if all values from the
+        * other are already added
+        */
+        while (i <= mid) {
+            newArr[location] = arr[i];
             i++;
             location++;
             swaps++;
         }
-        while (j <= end)
-        {
-            newArr[location] = list[j];
+        while (j <= end) {
+            newArr[location] = arr[j];
             j++;
             location++;
             swaps++;
         }
-        for (int x = beg; x <= end; x++)
-        {
-            list[x] = newArr[x - beg];
+        
+        // Copies the sorted array to the original array
+        for (int x = start; x <= end; x++) {
+            arr[x] = newArr[x - start];
         }
     }
     
-    public static void testBubble()
-    {
-        int[] list = {5, 1, 50, 2, 3, 182, 12, 97, 14, 9};
-        for (int a : list)
-        {
+    public static void testBubble() {
+        int[] arr = {5, 1, 50, 2, 3, 182, 12, 97, 14, 9};
+        for (int a : arr) {
             System.out.print(a + ",");
         }
         System.out.println();
-        bubbleSort(list);
-        for (int a : list)
-        {
-            System.out.print(a + ",");
-        }
-    }
-    
-    public static void testInsertion()
-    {
-        int[] list = {5, 1, 50, 2, 3, 182, 12, 97, 14, 9};
-        for (int a : list)
-        {
-            System.out.print(a + ",");
-        }
-        System.out.println();
-        insertionSort(list);
-        for (int a : list)
-        {
+        bubbleSort(arr);
+        for (int a : arr) {
             System.out.print(a + ",");
         }
     }
     
-    public static void testSelection()
-    {
-        int[] list = {5, 1, 50, 2, 3, 182, 12, 97, 14, 9};
-        for (int a : list)
-        {
+    public static void testInsertion() {
+        int[] arr = {5, 1, 50, 2, 3, 182, 12, 97, 14, 9};
+        for (int a : arr) {
             System.out.print(a + ",");
         }
         System.out.println();
-        selectionSort(list);
-        for (int a : list)
-        {
+        insertionSort(arr);
+        for (int a : arr) {
             System.out.print(a + ",");
         }
     }
     
-    public static void testMerge()
-    {
-        int[] list = {5, 1, 50, 2, 3, 182, 12, 97, 14, 9};
-        for (int a : list)
-        {
+    public static void testSelection() {
+        int[] arr = {5, 1, 50, 2, 3, 182, 12, 97, 14, 9};
+        for (int a : arr) {
             System.out.print(a + ",");
         }
         System.out.println();
-        mergeSort(list);
-        for (int a : list)
-        {
+        selectionSort(arr);
+        for (int a : arr) {
+            System.out.print(a + ",");
+        }
+    }
+    
+    public static void testMerge() {
+        int[] arr = {5, 1, 50, 2, 3, 182, 12, 97, 14, 9};
+        for (int a : arr) {
+            System.out.print(a + ",");
+        }
+        System.out.println();
+        mergeSort(arr);
+        for (int a : arr) {
             System.out.print(a + ",");
         }
     }
